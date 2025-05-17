614429c2c25f4c0c9605ef50359b090bb8a58308
Felipe Nunes <nullnes.sh@gmail.com>
2025-05-17 01:01:47 -0300

    feat: implement MapError async alternative noted due new code organization

--- src/Nullnes.Labs.Result/Result.MapError.cs
+++ src/Nullnes.Labs.Result/Result.MapError.cs
@@ -17,5 +17,12 @@ public sealed partial record Result<TSuccess, TError>
 
 public static partial class AsyncResultExtensions
 {
-    
+    [Pure]
+    public static async Task<Result<TSuccess, TNewError>> MapError<TSuccess, TError, TNewError>(
+        this Task<Result<TSuccess, TError>> resultTask,
+        Func<TError, TNewError> errorMapper) where TNewError : class, IError where TError : class, IError
+    {
+        Result<TSuccess, TError> awaitedResult = await resultTask;
+        return awaitedResult.MapError(errorMapper);
+    }
 }
